4e5df4eaf52ae97d244a784cc6d717daf4f1d16a
Paul Gellai <paul@paul.systems>
2019-02-24 14:37:04 -0700

    initial commit

---
+++ CSE-360-Assignment-2/src/cse360assign2/Calculator.java
@@ -0,0 +1,68 @@
+/*
+ * Name: Andrew Gellai
+ * Class ID: 382
+ * Assignment: Homework 1
+ * Description: A simple calculator class which contains basic arithmetic operations.
+ */
+
+package cse360assign2;
+
+public class Calculator {
+
+	private int total;
+	
+	public Calculator () {
+		total = 0;  // not needed - included for clarity
+	}
+	
+	/**
+	 * Returns total value in the calculator.
+	 * @param the value to be passed to be added
+	 * @return the total value of calculator
+	 */
+	public int getTotal () {
+		return 0;
+	}
+	
+	/**
+	 * Adds a value to the calculator total.
+	 * @param the value to be added to the total
+	 */
+	public void add (int value) {
+		
+	}
+	
+	/**
+	 * Subtracts a value from the calculator total.
+	 * @param the value to be subtracted from the total
+	 */
+	public void subtract (int value) {
+		
+	}
+	
+	/**
+	 * Multiply the calculator total by a value.
+	 * @param the value which the total is multiplied by
+	 */
+	public void multiply (int value) {
+		
+	}
+	
+	/**
+	 * Divides the calculator total by a value.
+	 * @param the value which the total is divided by
+	 * @return the total value of calculator
+	 */
+	public void divide (int value) {
+		
+	}
+	
+	/**
+	 * Returns history of all calculator actions
+	 * @param the value to be passed to be added
+	 * @return the total value of calculator
+	 */
+	public String getHistory () {
+		return "";
+	}
+}
